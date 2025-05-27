7a8e207c28db5bc750075411b51d4ca5e2d03af8
Florheim Wizard Mariano <fwizard0719@gmail.com>
2025-05-27 11:43:58 +0800

    this is exercise 1

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Form</title>
+</head>
+<body>
+    <style>
+        body {
+            width: 500px;
+            margin: 150px auto;
+            padding: 20px;
+            text-align: center;
+            font-size: 24px;
+            font-family: Arial, sans-serif;
+        }
+        button {
+            padding: 10px 20px;
+            font-size: 18px;
+            background-color: #c30010;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #ff2c2c;
+        }
+    </style>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        echo "Welcome back, " . htmlspecialchars($username) . "!";
+    }
+    ?>
+    <p>You want to Logout?</p>
+    <a href="login.html"><button>Logout</button></a>
+</body>
+</html>
